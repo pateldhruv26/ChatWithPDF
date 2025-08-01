150ccd09f757b6b561828f4528d263da459f02cb
Dhruv Patel <159645709+pateldhruv26@users.noreply.github.com>
2025-08-01 15:25:10 +0530

    Delete grammarCheck.py

--- grammarCheck.py
+++
@@ -1,8 +0,0 @@
-import language_tool_python
-from pyaspeller import YandexSpeller
-tool = language_tool_python.LanguageToolPublicAPI('en-US')
-
-def get_grammarCheck(text):
-    spellcheck = YandexSpeller().spelled(text)
-    correctedText = tool.correct(spellcheck)
-    return correctedText
